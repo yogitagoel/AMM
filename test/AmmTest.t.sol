7732e0ec4acdfc6f56fc752011b84a991d65bd95
yogitagoel <yogitagoel2006@gmail.com>
2025-03-21 23:27:26 +0530

    add

--- test/AmmTest.t.sol
+++ test/AmmTest.t.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from "../lib/forge-std/src/Test.sol";
 import {AMM} from "../src/AMM.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {DeployAmm} from "../script/AmmDeploy.s.sol";
-import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {ERC20Mock} from "../lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
 
 contract AmmTest is Test {
     DeployAmm deployer;
@@ -17,17 +17,31 @@ contract AmmTest is Test {
     uint initial_reserveA = 1000 * 1e18;
     uint initial_reserveB = 1000 * 1e18;
 
-    address public user1 = makeAddr("user1");
-    address public user2 = makeAddr("user2");
+    address public user = makeAddr("user");
 
     function setUp() external {
+
         deployer = new DeployAmm();
         (amm, tokenA, tokenB) = deployer.run();
 
-        tokenA.transfer(user1, 1e20);
-        tokenB.transfer(user1, 1e20);
-        tokenA.approve(user1, 1e20);
-        tokenB.approve(user1, 1e20);
+        console.log("TokenA Address:", address(tokenA));
+console.log("TokenB Address:", address(tokenB));
+console.log("AMM Address:", address(amm));
+
+
+        tokenA.mint(address(this),100 ether);
+        tokenB.mint(address(this),100 ether);
+
+        tokenA.mint(user,100 ether);
+        tokenB.mint(user,100 ether);
+
+        tokenA.approve(address(amm), type(uint256).max);
+        tokenB.approve(address(amm), type(uint256).max);
+
+        vm.startPrank(user);
+        tokenA.approve(address(amm), type(uint256).max);
+        tokenB.approve(address(amm), type(uint256).max);
+        vm.stopPrank();
 
         amm.addLiquidity(initial_reserveA, initial_reserveB);
     }
@@ -35,44 +49,44 @@ contract AmmTest is Test {
     function testAddLiquidity(uint256 amountA, uint256 amountB) external {
         vm.assume(amountA > 0);
         vm.assume(amountB > 0);
-        vm.prank(user1);
+        vm.prank(user);
         tokenA.approve(address(amm), 1e20);
         tokenB.approve(address(amm), 1e20);
-        vm.prank(user1);
+        vm.prank(user);
         amm.addLiquidity(amountA, amountB);
     }
 
     function testRemoveLiquidity(uint256 liquidity) external {
-        vm.prank(user1);
+        vm.prank(user);
         amm.removeLiquidity(liquidity);
     }
 
     function testSwapAtoB(uint256 amountA) external {
         vm.assume(amountA > 0);
-        vm.prank(user1);
+        vm.prank(user);
         tokenA.approve(address(amm), amountA);
         amm.swapAtoB(amountA);
     }
 
     function testSwapBtoA(uint256 amountB) external {
         vm.assume(amountB > 0);
-        vm.prank(user1);
+        vm.prank(user);
         tokenB.approve(address(amm), amountB);
         amm.swapBtoA(amountB);
     }
 
     function testGetReserve() external {
-        vm.prank(user1);
+        vm.prank(user);
         amm.getReserves();
     }
 
     function testGetBalance(address token) external {
-        vm.prank(user1);
+        vm.prank(user);
         amm.getBalance(token);
     }
 
     function testGetLiquidity() external {
-        vm.prank(user1);
+        vm.prank(user);
         amm.getLiquidity();
     }
 
