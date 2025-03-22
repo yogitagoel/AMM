885bfc3f593412bc12e8b253c4445754dd6cfe4c
yogitagoel <yogitagoel2006@gmail.com>
2025-03-22 11:37:56 +0530

    debugging

--- src/AMM.sol
+++ src/AMM.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.28;
 
 import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ReentrancyGuard} from "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
@@ -29,7 +29,7 @@ contract AMM is ReentrancyGuard{
     function addLiquidity(
         uint256 _amountA,
         uint256 _amountB
-    ) external nonReentrant {
+    ) external {
         require(_amountA > 0 && _amountB > 0, "Insufficient amount");
 
         require(
--- test/AmmTest.t.sol
+++ test/AmmTest.t.sol
@@ -14,8 +14,9 @@ contract AmmTest is Test {
     ERC20Mock public tokenA;
     ERC20Mock public tokenB;
 
-    uint initial_reserveA = 100 * 1e18;
-    uint initial_reserveB = 100 * 1e18;
+    uint256 public constant INITIAL_RESERVEB= 100 * 1e18;
+    uint256 public constant INITIAL_RESERVEA = 100 * 1e18;
+    uint256 public constant INITIAL_BALANCE=5000 ether;
 
     address public user = makeAddr("user");
 
@@ -25,11 +26,11 @@ contract AmmTest is Test {
         (amm, tokenA, tokenB) = deployer.run();
 
 
-        tokenA.mint(address(this),1000 ether);
-        tokenB.mint(address(this),1000 ether);
+        tokenA.mint(address(this),INITIAL_BALANCE);
+        tokenB.mint(address(this),INITIAL_BALANCE);
 
-        tokenA.mint(user,1000 ether);
-        tokenB.mint(user,1000 ether);
+        tokenA.mint(user,INITIAL_BALANCE);
+        tokenB.mint(user,INITIAL_BALANCE);
 
         tokenA.approve(address(amm), type(uint256).max);
         tokenB.approve(address(amm), type(uint256).max);
@@ -39,22 +40,29 @@ contract AmmTest is Test {
         tokenB.approve(address(amm), type(uint256).max);
         vm.stopPrank();
 
-        amm.addLiquidity(initial_reserveA, initial_reserveB);
+        amm.addLiquidity(INITIAL_RESERVEA, INITIAL_RESERVEB);
     }
 
     function testAddLiquidity(uint256 amountA, uint256 amountB) external {
-        vm.assume(amountA > 0);
-        vm.assume(amountB > 0);
+
+        vm.assume(amountA > 0 && amountA <= 1e22);
+        vm.assume(amountB > 0 && amountB <= 1e22);
+
+        console.log(amountA);
+        console.log(amountB);
+
         vm.prank(user);
         amm.addLiquidity(amountA, amountB);
         (uint256 reserveA , uint256 reserveB) = amm.getReserves();
-        assert(reserveA==initial_reserveA+amountA);
-        assert(reserveB==initial_reserveB+amountB);
+        assert(reserveA==INITIAL_RESERVEA+amountA);
+        assert(reserveB==INITIAL_RESERVEB+amountB);
     }
 
     function testRemoveLiquidity(uint256 liquidity) external {
         vm.prank(user);
+        uint256 Iinitial_Liquidity =amm.getLiquidity();
         amm.removeLiquidity(liquidity);
+        assert(amm.getLiquidity()==Iinitial_Liquidity-liquidity);
     }
 
     function testSwapAtoB(uint256 amountA) external {
@@ -88,7 +96,7 @@ contract AmmTest is Test {
 
     function testConstantProduct() public {
         (uint256 reserveA, uint256 reserveB) = amm.getReserves();
-        uint256 beforeK = initial_reserveA * initial_reserveB;
+        uint256 beforeK = INITIAL_RESERVEA * INITIAL_RESERVEB;
         uint256 afterK = reserveA * reserveB;
         assert(beforeK == afterK);
     }
